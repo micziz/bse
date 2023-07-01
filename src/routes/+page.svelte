86c59e766c8996c1aba797afedc685f938ed5e5e
micziz <miczicontent@gmail.com>
2023-07-01 21:11:51 +0200

    Add font and change input

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,13 +10,29 @@
 
 <h1 class="h1">Basketball Search Engine</h1>
 
-<form on:submit|preventDefault={redirect}>
-    <input type="text" bind:value={query}>
-</form>
+<div class="form-container">
+    <form on:submit|preventDefault={redirect}>
+        <input class="form" type="text" bind:value={query}>
+    </form>
+</div>
 
 
 <style>
     .h1{
         text-align: center;
+    }
+
+    .form-container{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .form{
+        width: 40rem;
+        height: 2rem;
+        border-radius: 5rem;
+        font-size: large;
+        border: 1px solid #ccc;
     }
 </style>
